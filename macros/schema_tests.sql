6ca0e99da439c2859c3a2259dd6a37521ce788ef
Tim Potter <tpot@frungy.org>
2025-09-20 14:06:39 +1000

    Add not_empty data test for all models

---
+++ macros/schema_tests.sql
@@ -0,0 +1,13 @@
+-- NON_EMPTY
+{% macro test_not_empty(model, column_name) %}
+    {{ return(adapter.dispatch('test_not_empty', 'mybi_dbt_core') (model, column_name)) }}
+{% endmacro %}
+
+{% macro default__test_not_empty(model, column_name) %}
+
+    select
+        count(1) as row_count
+    from {{ model }}
+    having count(1) = 0
+
+{% endmacro %}
